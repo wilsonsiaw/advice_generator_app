2a59e4b73c0c53b7cf68c226ae53a0deb16e6bdd
Wilson Siaw <wilsonsiawse@gmail.com>
2024-04-22 10:47:56 +0200

    ✨ feat(Application): Add content
    Add the JSX and placeholder content for the application

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import { useState } from 'react'
+import { useState } from 'react' 
 import './App.css'
+import dice from "../public/images/icon-dice.svg"
 
 function App() {
   
 
   return (
-    <>
-      
-    </>
+    <div className='advice-container'>
+      <h1 className='advice-number'>ADVICE #117</h1>
+      <div className='advice-text'>
+        <p>
+          "It is easy to sit up and take notice, 
+          what's dificult is getting up and taking action."
+        </p>
+      </div>
+      <div className='dice-container'>
+        <img src={dice} alt="" />
+      </div>
+    </div>
   )
 }
 
